86b7adf523c50c700b1f42ec2409e770ce829620
p-jacquot <pierre.jacquot@insa-lyon.fr>
2021-03-03 16:29:36 +0100

    Run bench with an unikernel.

---
+++ run/unikernel-run.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+unikernel_launch=$1
+bench=$2
+bench_args=$3
+
+if [ ! -e results ]; then
+    mkdir results
+fi
+
+if [ -e results/$bench ]; then
+    echo -e "WARNING : There is already a folder results/$bench."
+    echo "Please check what's inside, and remove it manually to avoid making mistakes."
+    echo "Aborting script."
+    exit
+fi
+
+mkdir results/$bench
+results_path=$(pwd)/results/$bench
+cd ../bin
+
+echo -e "Performing bench $bench :"
+
+for executable in $bench.*; do
+    echo -e "\t Executing $executable..."
+    "$unikernel_launch" $executable $bench_args > $results_path/$executable.log
+done
